dd460d7112429c3a16b9be1a8edfce19af0a4352
katiejg <jng.katie@gmail.com>
2018-07-23 01:45:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,25 @@
+class Song 
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@artists << artist
+    @@genres << genre
+    @@count += 1
+  end
+  def self.count
+    @@count
+  end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres 
+    @@genres.uniq
+  end
+end
+
 
