66b4144de0f9b458be7caba40499cc63b74d2708
katiejg <jng.katie@gmail.com>
2018-07-23 01:56:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,5 +31,16 @@ class Song
     end
     return genre_count
 end
+def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    return genre_count
+end
 end
 
